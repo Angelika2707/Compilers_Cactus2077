1ac9e97552df74e0be93da7fd856e87ebf7df51c
WellNotWell <olesya.gredyushko4956@gmail.com>
2024-11-03 21:44:51 +0300

    feat: update Parameter

--- src/main/java/ast/function/Parameter.java
+++ src/main/java/ast/function/Parameter.java
@@ -1,16 +1,34 @@
 package ast.function;
 
 import ast.base.ASTNode;
+import ast.expression.Expression;
 import ast.type.Type;
 import ast.visitor.Visitor;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Parameter extends ASTNode {
-    private String identifier;
-    private Type type;
+    private final String identifier;
+    private final Type type;
+    private final Expression expression;
+
+    public Parameter(String identifier, Type type) {
+        this.identifier = identifier;
+        this.type = type;
+        this.expression = null;
+    }
+
+    public Parameter(Expression expression) {
+        this.identifier = null;
+        this.type = null;
+        this.expression = expression;
+    }
+
+    public Parameter(String identifier) {
+        this.identifier = identifier;
+        this.type = null;
+        this.expression = null;
+    }
 
     @Override
     public void accept(Visitor visitor) {
