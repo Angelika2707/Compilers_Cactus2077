22f9f637ccd7bf0cef0b833ee9ca7d00292dbe50
Angelika2707 <53828140+Angelika2707@users.noreply.github.com>
2024-11-03 19:21:42 +0300

    modify: function classes

--- src/main/java/ast/function/Function.java
+++ src/main/java/ast/function/Function.java
@@ -11,14 +11,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Function extends ProgramUnit {
-    private String id;
+    private String identifier;
     private ParamList paramList;
     private Type returnType;
     private DeclarationList declarationList;
     private StatementList statementList;
 
-    @Override
     public void accept(Visitor visitor) {
+        this.paramList.accept(visitor);
         visitor.visit(this);
     }
 }
