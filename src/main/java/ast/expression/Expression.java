1138aa183b1eef32e3ba4b5c39594a312d0374c4
WellNotWell <olesya.gredyushko4956@gmail.com>
2024-11-03 17:13:14 +0300

    fix: expression abstract class

--- src/main/java/ast/expression/Expression.java
+++ src/main/java/ast/expression/Expression.java
@@ -1,32 +1,7 @@
 package ast.expression;
 
 import ast.base.ASTNode;
-import ast.visitor.Visitor;
 import lombok.Getter;
 
 @Getter
-public class Expression extends ASTNode {
-    public enum Operator {
-        PLUS, MINUS, MUL, DIV, MOD, EQ, NE, GT, LT, GE, LE, AND, OR, XOR, NOT
-    }
-
-    private Operator operator;
-    private Expression leftOperand;
-    private Expression rightOperand;
-    private Object value;
-
-    public Expression(Operator operator, Expression leftOperand, Expression rightOperand) {
-        this.operator = operator;
-        this.leftOperand = leftOperand;
-        this.rightOperand = rightOperand;
-    }
-
-    public Expression(Object value) {
-        this.value = value;
-    }
-
-    @Override
-    public void accept(Visitor visitor) {
-        visitor.visit(this);
-    }
-}
+public abstract class Expression extends ASTNode {}
