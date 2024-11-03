c1ec8c028875b86ac7948500541e8c9753c39f5f
WellNotWell <olesya.gredyushko4956@gmail.com>
2024-11-03 15:48:40 +0300

    fix: make abstract classes

--- src/main/java/ast/statement/Statement.java
+++ src/main/java/ast/statement/Statement.java
@@ -1,18 +1,5 @@
 package ast.statement;
 
 import ast.base.ProgramUnit;
-import ast.expression.Expression;
-import ast.visitor.Visitor;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class Statement extends ProgramUnit {
-    private Expression expression;
-
-    @Override
-    public void accept(Visitor visitor) {
-        visitor.visit(this);
-    }
-}
+public abstract class Statement extends ProgramUnit {}
--- src/main/java/ast/type/Type.java
+++ src/main/java/ast/type/Type.java
@@ -1,16 +1,6 @@
 package ast.type;
 
 import ast.base.ASTNode;
-import ast.visitor.Visitor;
-import lombok.Getter;
 
-@Getter
-public class Type extends ASTNode {
-    private String name;
-
-    @Override
-    public void accept(Visitor visitor) {
-        visitor.visit(this);
-    }
-}
+public abstract class Type extends ASTNode {}
 
